fad4d9500a36c3d3cc8cb165b58b8c5cfd308f6d
MuradMT <mammadzade03@gmail.com>
2024-10-19 12:09:14 +0100

    Learned about kotlin conditionals, conditional expressions and scanner class.

--- src/Main.kt
+++ src/Main.kt
@@ -1,28 +1,58 @@
+import java.util.Scanner
+
 fun main(args:Array<String>) {
     //region Data types and Variables
-    println("Hello World!")
-    val num:Int=4;//immutable variable,used explicit type specification
-    //num++ can not be reassigned
-    var mynum:Int=5;//mutable variable
-    mynum++
-    println("Final num is $mynum")
-
-    //Data types
-    val a=346L; //long
-    val b=34.6; //double
-    val c=34.5F; //float
-    val d="Hello"; //string
+//    println("Hello World!")
+//    val num:Int=4;//immutable variable,used explicit type specification
+//    //num++ can not be reassigned
+//    var mynum:Int=5;//mutable variable
+//    mynum++
+//    println("Final num is $mynum")
+//
+//    //Data types
+//    val a=346L; //long
+//    val b=34.6; //double
+//    val c=34.5F; //float
+//    val d="Hello"; //string
 
     //endregion
     //region Loops
-    val name="Murad"
-    for(i in 1..10){
-        println(name)
-    }
-    for(j in 1 until 10) println(name)
+//    val name="Murad"
+//    for(i in 1..10){
+//        println(name)
+//    }
+//    for(j in 1 until 10) println(name)
+//
+//    for(k in 1..10 step 2){
+//        println(name+" hi")
+//    }
+    //endregion
+    //region Conditionals and Conditional Expressions
+//    print("I print to the same line")
+//    val name= readLine()
+//    if(name=="Linus Torvalds"){
+//        println("You invented Linux OS")
+//    }
+//    else{
+//        println("You are just some regular guy")
+//    }
+//
+//   val reader=Scanner(System.`in`)
+//    val numb=reader.nextInt()
+//    val result=if(numb>0) "Positive"
+//    else if(numb<0) "Negative"
+//    else "Zero"
+//    println(result)
+//
+//    val num=reader.nextInt()
+//    when(num%2==0){
+//        true->println("Even number")
+//        false->{
+//            println("Odd number")
+//            println("Two line example")
+//        }
+//        else-> println("Not included")
+//    }
 
-    for(k in 1..10 step 2){
-        println(name+" hi")
-    }
     //endregion
 }
